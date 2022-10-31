ffa4456d64a648900e4e67f9e466517c1548f827
GordonOtieno <otienogordon95@gmail.com>
2022-10-31 16:06:55 +0300

    Refactor person class

--- person.rb
+++ person.rb
@@ -1,12 +1,13 @@
 class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission = 'true')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  attr_accessor :name, :age
-  attr_reader :id
 
   def can_use_services?
     of_age? || @parent_permission
