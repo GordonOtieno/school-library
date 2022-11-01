c0b78eb89200b095a72706e2b172090be011433c
GordonOtieno <otienogordon95@gmail.com>
2022-11-01 21:59:59 +0300

    Modify namable class

--- person.rb
+++ person.rb
@@ -1,4 +1,5 @@
-class Person
+require_relative './nameable'
+class Person <Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -7,11 +8,15 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
 
   private
 
