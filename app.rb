c21dea365c37081199d2df637806c1064a17d07b
GordonOtieno <otienogordon95@gmail.com>
2022-11-08 11:27:46 +0300

    Move main menu to app.rb

--- app.rb
+++ app.rb
@@ -94,5 +94,40 @@ class App
       end
     end
   end
+  def main_menu
+    puts ' '
+    puts 'Please choose an option by entering a number:
+        1 - List all books
+        2 - List all people
+        3 - Create a person
+        4 - Create a book
+        5 - Create a rental
+        6 - List all rentals for a given person id
+        7 - Exit'
+    puts ' '
+  end
+
+  def menu_selection
+    main_menu
+    selection = gets.chomp.to_i
+    case selection
+    when 1
+      list_all_books
+    when 2
+      list_all_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_all_rentals
+    when 7
+      puts 'bye bye see you later 👋'
+      exit
+    end
+    menu_selection
+  end
 
 end
