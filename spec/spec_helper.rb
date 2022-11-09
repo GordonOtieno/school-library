01cc2194d811a0461eba181b3f82529929615b15
fullstop125 <hassanmoses125@gmail.com>
2022-11-09 23:39:39 +0300

    create helper file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require_relative '../person'
+require_relative '../classroom'
+require_relative '../book'
+require_relative '../rental'
+require_relative '../student'
+require_relative '../teacher'
+require_relative '../capitalize_decorator'
+require_relative '../nameable'
+require_relative '../trimmer_decorator'
+
+
